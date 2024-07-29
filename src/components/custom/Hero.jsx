d15e3559f978e81069a28ce961ee31d9c7844c29
Chandan Kumar <chandankrr.91@gmail.com>
2024-07-29 13:27:15 +0530

    Add mockup image in landing page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -3,15 +3,15 @@ import { Button } from '../ui/button';
 
 const Hero = () => {
   return (
-    <div className="flex items-center h-full px-7 sm:px-10 md:px-32 lg:px-56 xl:px-72 flex-col gap-9 mb-16">
-      <h1 className="font-extrabold text-[42px] sm:text-[48px] md:text-[52px] text-center mt-16">
+    <div className="flex flex-col items-center h-full mb-16 px-7 sm:px-10 md:px-32 lg:px-56 xl:px-72 gap-7">
+      <h1 className="font-extrabold text-[42px] sm:text-[48px] text-center mt-16">
         <span className="text-[#f56555]">
           Discover Your Next Adventure with AI:
         </span>{' '}
         Personalized Itineraries at Your Fingertips
       </h1>
 
-      <p className="line-clamp-3 text-lg sm:text-xl text-gray-500 text-center">
+      <p className="text-base text-center text-gray-500 line-clamp-3 sm:text-xl">
         Your personal trip planner and travel curator creates custom itineraries
         tailored to your interests and budget. Enjoy hidden gems, popular
         attractions, and detailed schedules with tips for a seamless vacation.
@@ -20,6 +20,8 @@ const Hero = () => {
       <Link to={'/create-trip'}>
         <Button>Get Started, It&apos;s Free</Button>
       </Link>
+
+      <img className="mt-12" src="/mockup.png" alt="tripyz mockup" />
     </div>
   );
 };
